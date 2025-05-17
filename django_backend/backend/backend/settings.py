44c647bfcbdd27c3ed38cf49819424089f023c9b
JJ Wanda <jjwanda@gmail.com>
2025-05-17 01:03:29 -0400

    Forgot like...the most imortant p art

--- django_backend/backend/backend/settings.py
+++ django_backend/backend/backend/settings.py
@@ -138,5 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:5173",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "scavenger.wanda.party",
 ]
